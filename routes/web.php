3d36d7e50262a85916996a9443c74c7289bfd434
Hayk <h.balasanyan93@gmail.com>
2022-03-31 17:14:48 +0400

    docker config

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $client = \Elasticsearch\ClientBuilder::create()
+        ->build();
     \App\Models\Person::addAllToIndex();
     return view('welcome');
 });
